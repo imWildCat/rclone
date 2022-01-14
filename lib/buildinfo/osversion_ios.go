e483c413a2c181782f1499ab0e7e809f5201cde5
WildCat <wildcat.name@gmail.com>
2022-01-15 07:43:07 +0800

    build: drop gopsutil for iOS

---
+++ lib/buildinfo/osversion_ios.go
@@ -0,0 +1,10 @@
+//go:build ios
+// +build ios
+
+package buildinfo
+
+// GetOSVersion returns OS version, kernel and bitness
+func GetOSVersion() (osVersion, osKernel string) {
+
+	return "ios", "unknown"
+}
